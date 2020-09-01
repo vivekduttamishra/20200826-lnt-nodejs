7a63294c385bff50f93a393ae9144a34fc3c5ac3
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2020-09-01 11:40:26 +0530

    day#6 update#2

---
+++ archive/booksweb-client-version1/src/app/books-web-footer.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core'
+
+@Component({
+    selector: 'app-booksweb-footer',
+    template: 
+        `
+        <div class='footer'>&copy;vivek@conceptarchitect.in</div>
+        
+        `
+})
+export class BooksWebFooterComponent{
+}
